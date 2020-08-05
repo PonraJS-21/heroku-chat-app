33308417d950b2428521351f085ce935c7689f82
PonraJS-21 <sponraj121@gmail.com>
2020-08-05 13:10:30 +0530

    f

--- chatbot.py
+++ chatbot.py
@@ -12,8 +12,8 @@ from os import path
 import pickle
 
 
-physical_devices = tf.config.list_physical_devices('GPU') 
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+# physical_devices = tf.config.list_physical_devices('GPU') 
+# tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 with open("intents.json") as file:
     data = json.load(file)
