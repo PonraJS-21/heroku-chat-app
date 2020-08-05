d89717ecd3fe0a6a329246f533c402c662aca5b0
PonraJS-21 <sponraj121@gmail.com>
2020-08-05 12:41:58 +0530

    first

---
+++ app.py
@@ -0,0 +1,29 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+from chatbot import return_chat
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    a = return_chat('Hi')
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    if request.method == "POST":
+        input_string = request.form['chat_input']
+        if bool(input_string.strip()):
+            chat_response = return_chat(str(input_string))
+            return chat_response
+        else:
+            return 'Please enter some data'
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ chatbot.py
@@ -0,0 +1,122 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tensorflow as tf
+from keras.layers import Dense, LSTM
+from keras.models import Sequential, load_model
+import random
+import json
+from os import path
+import pickle
+
+
+physical_devices = tf.config.list_physical_devices('GPU') 
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            print('Words:  ',wrds)
+            words.extend(wrds)
+            print('extended:  ', words)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+        training.append(bag)
+        output.append(output_row)
+
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+
+modelName = 'model/model.h5'
+
+
+if path.exists('model/model.h5'):
+    
+    model = load_model(modelName)
+    print('Model Loaded')
+else:
+    model = Sequential()
+    model.add(Dense(100, input_shape=(32,  )))
+    model.add(Dense(100))
+    model.add(Dense(len(output[0]), activation="softmax"))
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+    model.summary()
+    
+    model.fit(training, output, epochs=100)
+    model.save(modelName)
+    model = load_model(modelName)
+
+
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+            
+    return numpy.array(bag)
+
+
+def return_chat(inp):    
+    usr_input = numpy.asarray([bag_of_words(inp, words)])
+    results = model.predict(usr_input)
+    results_index = numpy.argmax(results)
+    tag = labels[results_index]
+
+    for tg in data["intents"]:
+        if tg['tag'] == tag:
+            responses = tg['responses']
+
+    return (random.choice(responses))
